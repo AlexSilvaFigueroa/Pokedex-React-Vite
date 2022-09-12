9ef388664169c97587b12d64d9cdc63417ac87e7
Study <study@CLRTLC02G42FYMD6N.local>
2022-09-11 22:55:12 -0300

    feat: add gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import * as path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/Pokedex-React-Vite/",
   resolve: {
     alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
   },
